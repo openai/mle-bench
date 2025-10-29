273c5b7663c0add8e88dd4818678e289db0bdc06
hstern011 <hstern.data@gmail.com>
2025-10-29 13:35:43 +0000

    Add files for change of fork

---
+++ mlebench/competitions/custom-task/prepare.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from sklearn.model_selection import train_test_split
+
+from mlebench.utils import read_csv
+
+
+def prepare(raw: Path, public: Path, private: Path):
+    pass
